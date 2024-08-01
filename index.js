b6fe5cae8cec93b9e7045bb02aa273e7321553b1
LRlaxmirathod <168898815+LRlaxmirathod@users.noreply.github.com>
2024-08-01 11:28:24 +0530

    MAP METHOD
    MAP METHOD

---
+++ index.js
@@ -0,0 +1,21 @@
+//using map method double the array
+
+let arr = [1,2,3,4,5,6,7]
+arr.map((val)=>{
+    console.log(val *2 )
+})
+
+//using map method triple the array
+
+let array = [1,2,3,4,5,6,7,8]
+array.map((val)=>{
+    console.log(val * 3)
+})
+
+
+//using map method print binary number
+
+let arra = [1,2,3,4,5,7,8]
+arra.map((val)=>{
+    console.log(val.toString(2))
+})
